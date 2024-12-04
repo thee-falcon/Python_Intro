3c0e4cc36af227753f3412451c7a99db35bb4297
thee-falcon <omar.makran.10@gmail.com>
2024-12-04 23:57:07 +0100

    receive data from user

---
+++ receive_input.py
@@ -0,0 +1,12 @@
+# To Receive a Data from a User, we use the => input() Function, takes a Message String that show in thr terminal
+
+# we use a variable to store the return of the function
+
+receive = input('Waht is your name?\n=> ')
+
+print("Your Enterd the: " + receive)
+
+full_name = input("Please Enter your Full Name: ")
+favorite_color = input("Please Enter your Favorite Color: ")
+
+print(full_name + " Likes the " + favorite_color + " Color.")
