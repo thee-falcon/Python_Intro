e09e7774f7540e9f895137b566e2d3479be3c785
thee-falcon <omar.makran.10@gmail.com>
2024-12-05 01:56:28 +0100

    fortmated string

---
+++ formated_strings.py
@@ -0,0 +1,10 @@
+first = 'Omar'
+last = 'Makran'
+
+message = first + ' [' + last +  '] is a Coder'
+
+msg = f'{first} [{last}] is a Coder'
+
+print("String: " + message)
+print('--------------------------------------------')
+print("Formated String: " + msg)
