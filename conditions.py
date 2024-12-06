27325f7a8249a591043f5d1e84d9aa1656e713da
thee-falcon <omar.makran.10@gmail.com>
2024-12-06 20:20:53 +0100

    conditions is done

---
+++ conditions.py
@@ -0,0 +1,24 @@
+is_hot = True
+is_cold = True
+
+if is_hot:
+    print("I know the concept of the conditions, just I learn the syntax")
+    print("So drink some plenty of water")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
+
+print("----------------------")
+
+price_house = 1000000
+is_good_credit_card = False
+
+if is_good_credit_card:
+    print("You should down 10%")
+    print(str((10000 / 10) * 100) + " MAD")
+else:
+    print("You need to put down 20%")
+    print(str((10000 / 20) * 100) + " MAD")
