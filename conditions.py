363cb95a94bd118045f6e5b17700398e485ed705
thee-falcon <omar.makran.10@gmail.com>
2024-12-06 20:40:56 +0100

    conditions and logical operator

--- conditions.py
+++ conditions.py
@@ -1,7 +1,7 @@
 is_hot = True
 is_cold = True
 
-if is_hot:
+if is_hot or False:
     print("I know the concept of the conditions, just I learn the syntax")
     print("So drink some plenty of water")
 elif is_cold:
@@ -16,7 +16,7 @@ print("----------------------")
 price_house = 1000000
 is_good_credit_card = False
 
-if is_good_credit_card:
+if is_good_credit_card and True:
     print("You should down 10%")
     print(str((10000 / 10) * 100) + " MAD")
 else:
