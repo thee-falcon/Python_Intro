a31c382936db34c8b1633934371f5cc60849e841
thee-falcon <omar.makran.10@gmail.com>
2024-12-06 19:51:38 +0100

    math function concept is done

---
+++ math_function.py
@@ -0,0 +1,18 @@
+# we import a math 'module', a module is like a store and the store has a many sections like, fruit,vegetables, cleaning materials...
+# basicly that a simple example to understand the concept, but he is actualy like a separated file, inside this file has a scope of code (Functions)  
+import math
+
+x = 3.9
+
+round = round(x) # this will round it to 4
+print(round)
+
+y = -3.9
+abs = abs(y) # this will convert y to a positive integer
+print(abs)
+
+floor = math.floor(x) # will be floor it to become x = 3
+print(floor)
+
+ceil = math.ceil(x) # will be ceil it to become x = 4
+print(ceil)
