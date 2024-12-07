3d113e4c9a3bd3b8c1918edab270465218c9d5ba
thee-falcon <omar.makran.10@gmail.com>
2024-12-07 23:57:52 +0100

    last version while loop concept using guess game

--- while_loops.py
+++ while_loops.py
@@ -1,12 +1,19 @@
 import random
-import sys
+
+# ANSI escape codes for colors
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+MAGENTA = "\033[35m"
+CYAN = "\033[36m"
+RESET = "\033[0m"  # Resets color to default
 
 # Let's play a guessing game to practice what we've learned
-
-print(" ------------------------------------")
-print("|    Welcome to Guess Game Play 🎮   |")
-print(" ------------------------------------")
-print(" -----> Please Guess a number <------")
+print(CYAN + " ------------------------------------" + RESET)
+print(CYAN + "|    Welcome to Guess Game Play 🎮   |" + RESET)
+print(CYAN + " ------------------------------------" + RESET)
+print(YELLOW + " -----> Please Guess a number <------" + RESET)
 
 # Generate a random number
 min_size = 1
@@ -17,11 +24,12 @@ random_number = random.randint(min_size, max_size)
 attempts = 3
 
 while attempts > 0:
-    guess = input(f"You have {attempts} attempt(s) left. Guess a number: ")
+    print(BLUE + f"\nYou have {attempts} attempt(s) left." + RESET)
+    guess = input(MAGENTA + "Guess a number: " + RESET)
 
     # Check if the input is a valid digit
     if not guess.isdigit():
-        print("\033[31mPlease enter a valid number!\033[0m")
+        print(RED + "Please enter a valid number!" + RESET)
         continue  # Skip to the next iteration
     
     # Convert the guess to an integer
@@ -29,16 +37,15 @@ while attempts > 0:
     
     # Check the user's guess
     if guess == random_number:
-        print("🥳 OWWW WHAT A LEGEND YOU ARE 🔥🎇")
+        print(GREEN + "🥳 OWWW WHAT A LEGEND YOU ARE 🔥🎇" + RESET)
         break
     else:
-        print("Oops! Wrong guess. Try again.\n")
+        print(RED + "Oops! Wrong guess. Try again.\n" + RESET)
     
     # Decrement the number of attempts
     attempts -= 1
 
 # If the user runs out of attempts
 if attempts == 0:
-    print("Oooh Please don't cry 🥺")
-    print(f"The correct number was {random_number}. Good luck next time! 🤡")
-
+    print(RED + "Oooh Please don't cry 🥺" + RESET)
+    print(YELLOW + f"The correct number was {random_number}. Good luck next time! 🤡" + RESET)
