041099ca0f45bf2e053c4a838609ba376292d9fa
thee-falcon <omar.makran.10@gmail.com>
2024-12-10 23:51:16 +0100

    nested loop and solve a problem

--- nested_loop.py
+++ nested_loop.py
@@ -1,6 +1,29 @@
 # nested loop is a loop inside a loop
 
 # in function range, the number 4 is not included, so the while loop will stop in number 3
-for x in range(4):
-    for y in range(3):
-        print(f"(x:{x}, y:{y})")
+# for x in range(4):
+#     for y in range(3):
+#         print(f"(x:{x}, y:{y})")
+
+
+# Let's solve this problem:
+
+#  Output:
+# XXXXX
+# XX
+# XXXXX
+# XX
+# XX
+
+
+# Should Solve it using nested loop, yeah we can solve it using one for loop
+# but should solve it using nested loops
+
+# The solution
+numbers = [5, 2, 5, 2, 2]
+
+for i in numbers:
+    output = ''
+    for y in range(i):
+        output += 'X'
+    print(output)
