d3ee390baf5eb377efc9665e0b572113ffd4a9bd
thee-falcon <omar.makran.10@gmail.com>
2024-12-11 20:15:59 +0100

    tuples

---
+++ tuples.py
@@ -0,0 +1,8 @@
+# tuples is unlike the lists, we can't change the items, used to store collections of data.
+
+number = (1, 2, 3)
+
+print(number[0]) # will print the number 1
+
+my_tuple = ("apple", "banana", "cherry")
+print(my_tuple)
