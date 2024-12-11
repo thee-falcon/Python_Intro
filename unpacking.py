566db6283f5601735875a9027871345398c75973
thee-falcon <omar.makran.10@gmail.com>
2024-12-11 20:26:54 +0100

    unpacking

---
+++ unpacking.py
@@ -0,0 +1,16 @@
+# when creat a tuple, we normally assign values to it
+# But in python, we are also allowed to extract the values back into variables. this calles unpacking
+
+coordinates = (1, 2 , 3)
+
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+
+x, y, z = coordinates # this is a shorthand to achieve the same result of the top example initialize
+                      # and this is unpacking in the python, and we can also use it with listes
+
+print(x)
+print(y)
+print(z)
+
