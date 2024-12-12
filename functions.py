782724834ac99c1409755d3fb9d2fc6c311a313a
thee-falcon <omar.makran.10@gmail.com>
2024-12-12 23:26:54 +0100

    function with parameters and keyword arguments and return statments

--- functions.py
+++ functions.py
@@ -3,6 +3,11 @@
 def display_message_to_user(firs_name, last_name):
     print(f"Hi dear user: {firs_name} {last_name} <3")
 
+def square(number):
+    print(number * number)
+
 print("Start")
 display_message_to_user("Omar", last_name="Makran")
 print("Finish!")
+
+print(square(3)) # should print 9 and None because we don't add the return of the function, so python by default return None (NULL)
