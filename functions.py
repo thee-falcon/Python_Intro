7533c229c7caadb35971f2d9deb9016bcae605b1
thee-falcon <omar.makran.10@gmail.com>
2024-12-12 23:10:00 +0100

    function with parameters

--- functions.py
+++ functions.py
@@ -1,8 +1,8 @@
 # A function is a block of code which only runs when it us called. You can pass data, known as parameters, into a function.
 
-def display_message_to_user():
-    print("Hi dear user <3")
+def display_message_to_user(firs_name, last_name):
+    print(f"Hi dear user: {firs_name} {last_name} <3")
 
 print("Start")
-display_message_to_user()
+display_message_to_user("Omar", "Makran")
 print("Finish!")
