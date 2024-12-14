a02d15cc883da2e536806419752fd22c8b46c5d8
thee-falcon <omar.makran.10@gmail.com>
2024-12-14 23:27:37 +0100

    classes, constructor

--- classes.py
+++ classes.py
@@ -11,12 +11,19 @@ RESET = "\033[0m"  # Resets color to default
 
 # we declare a class names Point
 class Point:
+    # we declare a constructor
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
     def draw(self):
-        print("draw")
+        print(f"Draw => Point ({self.x}, {self.y})")
 
 # point is an object      
-point = Point()
+point = Point(1, 2)
 # print the type of the point
 print(type(point))
 # will print the boolan of isinstance method to check if point is an instance of the Point Class
-print(isinstance(point, int))
+print(isinstance(point, Point))
+
+point.draw()
