5a5ad3de23ba02e9ae880f29afc7d71dc5bcbf9f
thee-falcon <omar.makran.10@gmail.com>
2024-12-14 23:49:00 +0100

    class vs instance attributes

--- classes.py
+++ classes.py
@@ -11,6 +11,8 @@ RESET = "\033[0m"  # Resets color to default
 
 # we declare a class names Point
 class Point:
+
+    default_color = "red"
     # we declare a constructor
     def __init__(self, x, y) -> None:
         self.x = x
@@ -19,11 +21,18 @@ class Point:
     def draw(self):
         print(f"Draw => Point ({self.x}, {self.y})")
 
+# We access to the Class Directly and change the value of 'default color', so all Attributes well get the same default color that we change
+Point.default_color = "yellow"
+
 # point is an object      
 point = Point(1, 2)
 # print the type of the point
 print(type(point))
 # will print the boolan of isinstance method to check if point is an instance of the Point Class
 print(isinstance(point, Point))
-
+print(YELLOW + point.default_color + RESET)
 point.draw()
+
+another = Point(3, 4)
+print(YELLOW + another.default_color + RESET)
+another.draw()
