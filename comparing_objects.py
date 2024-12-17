487d9e206f10df9f961b5382df02d6b6c407fa50
thee-falcon <omar.makran.10@gmail.com>
2024-12-17 12:12:20 +0100

    arthmetic_operations

--- comparing_objects.py
+++ comparing_objects.py
@@ -8,9 +8,17 @@ class Point:
     
     def __gt__(self, object) -> bool:
         return self.x > object.x and self.y > object.y
+    
+    def __add__(self, object) -> int:
+        return Point(self.x + object.x, self.y + object.y)
+    
+    def __str__(self) -> str:
+        return f"{self.x, self.y}"
+
 
 point = Point(4, 24)
 other = Point(1, 2)
 
 print(point == other)
 print(point > other)
+print(point + other)
