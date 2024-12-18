40594f7dda5df1141eef5a8fdbe07f3153d482a7
thee-falcon <omar.makran.10@gmail.com>
2024-12-18 23:28:02 +0100

    start inheritance

---
+++ inheritance.py
@@ -0,0 +1,13 @@
+class Mammal:
+    def eat():
+        print("eat")
+    
+    def walk():
+        print("walk")
+
+class Fish:
+    def eat():
+        print("eat")
+    
+    def swim():
+        print("swim")
