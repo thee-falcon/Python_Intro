f2929ad5a9b1f5fc939f3edfd47a727bd01a276f
thee-falcon <omar.makran.10@gmail.com>
2024-12-23 20:22:20 +0100

    working with zip files are done

--- python_standard_library/working_with_zip_files.py
+++ python_standard_library/working_with_zip_files.py
@@ -9,3 +9,12 @@ with ZipFile("files.zip", "w") as zip:
 # if we want read the data of the zip files
 with ZipFile("files.zip") as zip:
     print(zip.namelist())
+    # if we want to get information about specify file in zip file
+    info = zip.getinfo("ecommerce/__init__.py")
+    print(info) # this will print all information about the file
+    # if we want a specifec information
+    print(info.file_size)
+    print(info.compress_size)
+    print(info.date_time)
+    # to extract all the files from the zip file
+    zip.extractall("extract")
