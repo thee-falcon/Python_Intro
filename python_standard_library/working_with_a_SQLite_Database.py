a7a68fe2b52733722c41ebc973e7354ef072db98
thee-falcon <omar.makran.10@gmail.com>
2024-12-25 21:50:59 +0100

    writen to data base, now I work for installin a sqlite data base

--- python_standard_library/working_with_a_SQLite_Database.py
+++ python_standard_library/working_with_a_SQLite_Database.py
@@ -4,6 +4,14 @@ import sqlite3
 import json
 from pathlib import Path
 
+# we load the data from movie json to movies variable
 movies = json.loads(Path("movies.json").read_text())
 
-print(movies)
+# we gonna creat a file for data base
+with sqlite3.connect("db.sqlite3") as con:
+    # ?: id and title and year
+    command = "INSERT INTO Movies VALUES(?, ?, ?)"
+    for movie in movies:
+        con.execute(command, tuple(movie.values()))
+    # now will be writen in data base
+    con.commit()
