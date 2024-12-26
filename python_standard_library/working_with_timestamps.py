4ce1799cc0f34291d711a7a9de5aad10871f95bb
thee-falcon <omar.makran.10@gmail.com>
2024-12-26 22:50:55 +0100

    working with timestamps is done

--- python_standard_library/working_with_timestamps.py
+++ python_standard_library/working_with_timestamps.py
@@ -1,3 +1,13 @@
 # we're going to work with date and time in python
 
-  
+import time
+
+def send_emails():
+    for i in range(1000):
+        print(f"email is send to the user{i}")
+
+start = time.time()
+send_emails()
+end = time.time()
+duration = end - start
+print(f"the duration of the time{duration}")
