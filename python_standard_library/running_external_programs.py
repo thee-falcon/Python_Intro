467c148e6e781082d72ce448c4873b02b1d2f626
thee-falcon <omar.makran.10@gmail.com>
2024-12-30 22:36:13 +0100

    running external programs

--- python_standard_library/running_external_programs.py
+++ python_standard_library/running_external_programs.py
@@ -1,1 +1,17 @@
 # we are going learn how to call external programs from our python scripts
+
+# with this module we can spot a child process, A process is basically an instance of a running program
+# so with this module we can run other  programs
+import subprocess
+
+try:
+   complited =  subprocess.run(["ls", "-l"],
+                               capture_output=True,
+                               text=True # to remove the 'b' prefex in stdout
+                               )
+   print("args: ", complited.args)
+   print("returncode: ", complited.returncode)
+   print("stderr: ", complited.stderr) # stderr if our code return a 1
+   print("stdout: ", complited.stdout) # stdout we use when we need to capture the output
+except subprocess.CalledProcessError as ex:
+    print(ex)
