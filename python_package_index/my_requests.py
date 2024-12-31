6bcd5a143f44129a1413420643ed1d035feb1d49
thee-falcon <omar.makran.10@gmail.com>
2024-12-31 20:30:55 +0100

    installing pipenv and removing the env

--- python_package_index/my_requests.py
+++ python_package_index/my_requests.py
@@ -2,3 +2,13 @@ import requests
 
 response = requests.get("http://google.com")
 print(response) # we got a response with a statu 200, which indicates succes.
+
+# to activate the enevironment and work with module requests
+# use the command: pipenv shell
+# when you run this command basically, you lunch a subshell in virtual enviroment, of the environment source path.
+
+# the compile your program using: python file_name.py
+
+
+# to knowing the path of the environment use the command:
+# pipenv --venv
