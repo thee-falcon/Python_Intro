81092e8110c54a57aaf997ee7d625b4bc3be10a1
thee-falcon <omar.makran.10@gmail.com>
2025-01-07 21:11:17 +0100

    working with pdf files and combine multiple pdfs into a single pdf

--- popular_python_packages/working_with_pdfs.py
+++ popular_python_packages/working_with_pdfs.py
@@ -14,3 +14,10 @@ with open("thefalcon_omakran.pdf", "rb") as file:
     # now let's write a new pdf file and add the rotate page(page) to it.
     with open("rotated.pdf", "wb") as output:
         writer.write(output)
+
+# now we are going to combine multiple pdfs into a single pdf
+file_names = ["thefalcon_omakran.pdf", "rotated.pdf"]
+merger = PyPDF2.PdfMerger()
+for file_name in file_names:
+    merger.append(file_name)
+merger.write("combine.pdf") # we create the new pdf
