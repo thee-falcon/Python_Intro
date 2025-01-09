eb8c7bf5cc0e6860c4841cc48e45afde573b6257
thee-falcon <omar.makran.10@gmail.com>
2025-01-09 19:53:01 +0100

    working with excel spreadsheets is done

--- popular_python_packages/working_with_excel_spreadsheets/app.py
+++ popular_python_packages/working_with_excel_spreadsheets/app.py
@@ -14,3 +14,17 @@ cell = sheet["a1"]
 #         cell = sheet.cell(row, col)
 #         print(cell.value)
 
+# if we want to access a range of cells, we can use this methods:
+column = sheet["a1"]
+cells = sheet["a1:c3"]
+print(cells)
+print(sheet[1:3])
+
+# now this sheet object has a few methods you should be aware of.
+# for example we use the append() method to add a row in the end of this sheet, and we have another methods also
+sheet.append([1, 2, 3])
+# sheet.insert_cols(0, 20)
+# sheet.insert_rows(2, 10)
+
+# and then we have to sve this workbook, and we gonna save it as a new file.
+wb.save("transaction2.xlsx")
